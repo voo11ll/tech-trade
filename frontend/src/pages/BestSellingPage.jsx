5a1e2fa7983114293eb1829a2fbb07b0568b04c1
vrungel <arsenyan.m.i20@gmail.com>
2024-07-05 22:26:19 +0300

    update best selling page

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -1,3 +1,48 @@
+// import React, { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import { useSearchParams } from "react-router-dom";
+// import Header from "../components/Layout/Header";
+// import Loader from "../components/Layout/Loader";
+// import ProductCard from "../components/Route/ProductCard/ProductCard";
+// import styles from "../styles/styles";
+// import Footer from "../components/Layout/Footer";
+
+// const BestSellingPage = () => {
+//   const [data, setData] = useState([]);
+//   const {allProducts,isLoading} = useSelector((state) => state.products);
+
+//   useEffect(() => {
+//     const allProductsData = allProducts ? [...allProducts] : [];
+//     const sortedData = allProductsData?.sort((a,b) => b.sold_out - a.sold_out); 
+//     setData(sortedData);
+//   }, [allProducts]);
+
+//   return (
+//    <>
+//    {
+//     isLoading ? (
+//       <Loader />
+//     ) : (
+//       <div>
+//       <Header activeHeading={2} />
+//       <br />
+//       <br />
+//       <div className={`${styles.section}`}>
+//         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+//           {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//     )
+//    }
+//    </>
+//   );
+// };
+
+// export default BestSellingPage;
+
+
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
@@ -9,34 +54,36 @@ import Footer from "../components/Layout/Footer";
 
 const BestSellingPage = () => {
   const [data, setData] = useState([]);
-  const {allProducts,isLoading} = useSelector((state) => state.products);
+  const { allProducts, isLoading } = useSelector((state) => state.products);
 
   useEffect(() => {
     const allProductsData = allProducts ? [...allProducts] : [];
-    const sortedData = allProductsData?.sort((a,b) => b.sold_out - a.sold_out); 
+    const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
     setData(sortedData);
   }, [allProducts]);
 
   return (
-   <>
-   {
-    isLoading ? (
-      <Loader />
-    ) : (
-      <div>
-      <Header activeHeading={2} />
-      <br />
-      <br />
-      <div className={`${styles.section}`}>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-          {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header activeHeading={2} />
+          <br />
+          <br />
+          <div className={`${styles.section}`}>
+            <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+              {data &&
+                data
+                  .slice()
+                  .reverse()
+                  .map((i, index) => <ProductCard data={i} key={index} />)}
+            </div>
+          </div>
+          <Footer />
         </div>
-      </div>
-      <Footer />
-    </div>
-    )
-   }
-   </>
+      )}
+    </>
   );
 };
 
